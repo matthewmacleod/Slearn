4a314da0bbf09050cd8aef627b517a10ae99b9b4
Matthew K. MacLeod <matthewmacleod28@gmail.com>
2016-01-22 22:45:30 -0700

    add basic functions

--- src/main/scala/slearn/Stats.scala
+++ src/main/scala/slearn/Stats.scala
@@ -3,7 +3,34 @@ package slearn
 object Stats {
     val something = "this is a string"
 
+    def abs(n: Int): Int =
+      if (n < 0) -n
+      else n
+
+
+    def mean(xs: List[Int]): Double = xs match {
+      case Nil => 0.0
+      case ys => ys.reduceLeft(_ + _) / ys.size.toDouble
+    }
+
+
+    def std(xs: List[Int], avg: Double): Double = xs match {
+      case Nil => 0.0
+      case ys => math.sqrt((0.0 /: ys) {
+        (a,e) => a + math.pow(e - avg, 2.0)
+      } / (xs.size-1))
+    }
+
+    def factorial(n: Int): Int = {
+      def go(n: Int, acc: Int): Int =
+        if (n <= 0) acc
+        else go(n-1, n*acc)
+
+      go(n, 1)
+    }
+
+
     def main(args: Array[String]) {
-          println("Running the First application.")
+          println("Running the Stats application.")
     }
 }
